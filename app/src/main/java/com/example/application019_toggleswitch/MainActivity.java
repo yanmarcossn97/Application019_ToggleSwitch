9a1021d8f3523bfffaea07aeb9ec0cd1d28063fb
Yan Marcos <yanmarcossn97@gmail.com>
2020-04-11 16:31:39 -0300

    Substituição do botão de salvar por um método que monitora mudanças

--- app/src/main/java/com/example/application019_toggleswitch/MainActivity.java
+++ app/src/main/java/com/example/application019_toggleswitch/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.application019_toggleswitch;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String statusSwitch = "";
+    private String statusToggle = "";
 
     private Switch switchSenha;
     private ToggleButton toggleSenha;
@@ -22,30 +26,46 @@ public class MainActivity extends AppCompatActivity {
         toggleSenha = findViewById(R.id.toggleSenha);
         viewResultado = findViewById(R.id.viewResultado);
 
-    }
-
-    public void salvarConfig(View view) {
-        String statusSwitch = "";
-        String statusToggle = "";
-
-        boolean estadoSwitch = switchSenha.isChecked();
-        boolean estadoToggle = toggleSenha.isChecked();
-
-        if(estadoSwitch) {
-            statusSwitch = "Switch ativado - ";
-            viewResultado.setText(statusSwitch);
-        } else {
-            statusSwitch = "Switch desativado - ";
-            viewResultado.setText(statusSwitch);
-        }
-
-        if(estadoToggle) {
-            statusToggle = "Toggle ativado - ";
-            viewResultado.setText(statusSwitch + statusToggle);
-        } else {
-            statusToggle = "Toggle desativado - ";
-            viewResultado.setText(statusSwitch + statusToggle);
-        }
+        monitoradorDeEstado();
 
     }
+
+    public void monitoradorDeEstado() {
+
+        switchSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                boolean estadoSwitch = switchSenha.isChecked();
+
+                if(estadoSwitch) {
+                    statusSwitch = "Switch ativado - ";
+                    viewResultado.setText(statusSwitch + statusToggle);
+                } else {
+                    statusSwitch = "Switch desativado - ";
+                    viewResultado.setText(statusSwitch + statusToggle);
+                }
+
+            }
+        });
+
+        toggleSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                boolean estadoToggle = toggleSenha.isChecked();
+
+                if(estadoToggle) {
+                    statusToggle = "Toggle ativado - ";
+                    viewResultado.setText(statusSwitch + statusToggle);
+                } else {
+                    statusToggle = "Toggle desativado - ";
+                    viewResultado.setText(statusSwitch + statusToggle);
+                }
+
+            }
+        });
+
+    }
+
 }
